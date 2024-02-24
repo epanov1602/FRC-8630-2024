4d9539961e73f068ede8abf06c5db0022dc09e88
Gene Panov <gene.panov@gmail.com>
2024-02-24 10:12:47 -0600

    Increased max speed. Watch out, this is not tested

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -65,7 +65,7 @@ public final class Constants {
     
     // Driving Parameters - Note that these are not the maximum capable speeds of
     // the robot, rather the allowed maximum speeds
-    public static final double kMaxSpeedMetersPerSecond = 0.36 * kPhysicalMaxSpeedMetersPerSecond;
+    public static final double kMaxSpeedMetersPerSecond = 0.52 * kPhysicalMaxSpeedMetersPerSecond;
     public static final double kMaxAngularSpeed = 0.5 * kPhysicalMaxAngularSpeedRadiansPerSecond; // radians per second
     public static final double kMaxRateLimitedAngularSpeed = 0.25 * kPhysicalMaxAngularSpeedRadiansPerSecond; // smaller turn speed when under human control
     public static final double kMaxAccelerationMetersPerSecondSquared = 0.6 * kPhysicalMaxAccelerationMetersPerSecondSquared;
